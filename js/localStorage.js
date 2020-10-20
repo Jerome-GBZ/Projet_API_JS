7d85608634ce87935564c5107435c9e538074fb9
Jerome GBZ <darkevent924@gmail.com>
2020-10-20 14:52:22 +0200

    add content on localstorage

--- js/localStorage.js
+++ js/localStorage.js
@@ -3,5 +3,23 @@ function save_LS() { // save local storage
 
 }
 
+if(localStorage.getItem("i") == null) {
+  localStorage.setItem("i", "0"); // initialiser
+}
 
-console.log(localStorage);
+let i = localStorage.getItem("i");
+i++;
+var key_LS = "favoris_"+i;
+
+
+localStorage.setItem(key_LS, "Titanic");
+localStorage.setItem("i", i);
+
+
+/*
+if(typeof localStorage!='undefined') {
+
+} else {
+  alert("localStorage n'est pas supporté ou vide...");
+}
+*/
