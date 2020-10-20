bb27c726be3615212dc75ab65bacb015e50fadd3
elChapo38000 <lorismartinez38@gmail.com>
2020-10-20 14:20:13 +0200

    API key

--- js/ajax.js
+++ js/ajax.js
@@ -1,2 +1,12 @@
+$affichage = $('#zone_affichage');
 
-$zone_affichage = 
+$btn_recherche = $('#btn-lancee-recherhe')
+
+$affichage.attr("onclick","recherche()");
+
+function recherche() {
+  var str_recherche = $affichage;
+  
+
+  https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction
+}
