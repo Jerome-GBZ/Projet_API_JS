868a146acbf3cedcefc2253b3bc2e419ba290c6c
Jerome GBZ <darkevent924@gmail.com>
2020-10-21 14:47:24 +0200

    Partie favoris fini

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,14 +1,9 @@
-// initialiser localStorage
-if(localStorage.getItem("fav") == null) {
-  let obj = JSON.stringify( {i:0} );
-  localStorage.setItem("fav", obj);
-}
-
+// initialiser la liste de favoris
 init_fav();
 
 
 function color_Etoile() { // Si un favoris est detecter dans zone affichage mettre etoile en pleine
-    if( localStorage.getItem( $("#zone_affichage").val() )  != null ) {
+    if( localStorage.getItem( "key_"+$("#zone_affichage").val() )  != null ) {
         // image etoile pleine
         $("#etoile_img").attr("src","./images/etoile-pleine.svg");
     } else {
@@ -25,46 +20,43 @@ function save_LS() { // save local storage quand on clique
     var zone_affichage = $("#zone_affichage");
 
     if( zone_affichage.val().length > 0 ) { // save
+      /*
         let obj = JSON.parse(localStorage.getItem("fav"));
         let indice = obj.i;
-
-
-        obj[indice] = {non:zone_affichage.val()};
+        console.log(obj);
+        obj[indice] = {nom:zone_affichage.val()};
         obj.i = indice+1;
 
         let final_obj = JSON.stringify( obj );
         localStorage.setItem("fav", final_obj);
+        */
+        localStorage.setItem("key_"+zone_affichage.val(), zone_affichage.val());
     }
+
+    // re Actualiser la liste des favoris à l'écran
+    init_fav();
 }
 
 function init_fav() {
-    // recup le localstorage
-    var zone_affichage = $("#zone_affichage");
-
     // parse le resultat du localStorage
-    let obj = JSON.parse(localStorage.getItem("fav"));
+    // let obj = JSON.parse(localStorage.getItem("fav"));
 
-    // placer les element d(obj dans les li 
-    console.log(obj);
+    $("#liste-favoris").text("");
+    $("#section-favoris").append('<p class="info-vide"></p>');
+    // placer les element d'obj dans les li
+    if( localStorage.length < 1 ) {
+        $("#section-favoris").append('<p class="info-vide">( &empty; Aucune recherche enregistrée )</p>');
+    } else {
+        for (var i=0; i < localStorage.length; i++) {
+            let clef = localStorage.key(i);
+            let nom = localStorage.getItem(clef);
+            $("#liste-favoris").append('<li> <span title="Cliquer pour relancer la recherche">'+nom+'</span> <img src="images/croix.svg" id="'+clef+'" onclick="remove_LS(this)" alt="Icone pour supprimer le favori" width=15 title="Cliquer pour supprimer le favori">');
+        }
+    }
 }
 
-// if(localStorage.getItem("i") == null) {
-  // localStorage.setItem("i", "0"); // initialiser
-// }
-
-// let i = localStorage.getItem("i");
-// i++;
-// var key_LS = "favoris_"+i;
-
-
-// localStorage.setItem("Titanic");
-// localStorage.setItem("i", i);
-
-
-/*
-if(typeof localStorage!='undefined') {
-
-} else {
-  alert("localStorage n'est pas supporté ou vide...");
+function remove_LS(img_click) {
+    // supprimer element dans le local storage
+    localStorage.removeItem(img_click.id);
+    init_fav();
 }
-*/
