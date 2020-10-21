eb3c1beef15f15d42ff16f8a0557cf1df7543818
Jerome GBZ <darkevent924@gmail.com>
2020-10-21 11:33:46 +0200

    modif localstorage

--- js/localStorage.js
+++ js/localStorage.js
@@ -9,28 +9,33 @@ function color_Etoile() { // Si un favoris est detecter dans zone affichage mett
 }
 
 function save_LS() { // save local storage quand on clique
-  // !! attention pas de doublon !!
+    // !! attention pas de doublon !!
 
-  // Reccuperer la zone_affichage en Jquery
-  var zone_affichage = $("#zone_affichage");
-  var content_zone = zone_affichage.val();
-  console.log( zone_affichage );
-  console.log( content_zone );
+    // Reccuperer la zone_affichage en Jquery
+    var zone_affichage = $("#zone_affichage");
+    var content_zone = zone_affichage.val();
+    // console.log( zone_affichage );
+    // console.log( content_zone );
 
+    // console.log(zone_affichage.val().length);
 
+    if( zone_affichage.val().length > 0 ) { // save
+        //
+        localStorage.setItem("", zone_affichage.val());
+    }
 }
 
-if(localStorage.getItem("i") == null) {
-  localStorage.setItem("i", "0"); // initialiser
-}
+// if(localStorage.getItem("i") == null) {
+  // localStorage.setItem("i", "0"); // initialiser
+// }
 
-let i = localStorage.getItem("i");
-i++;
-var key_LS = "favoris_"+i;
+// let i = localStorage.getItem("i");
+// i++;
+// var key_LS = "favoris_"+i;
 
 
-localStorage.setItem(key_LS, "Titanic");
-localStorage.setItem("i", i);
+// localStorage.setItem("Titanic");
+// localStorage.setItem("i", i);
 
 
 /*
